686d5f07a028df4e366a8dcc0d13fb1f7eac055e
joshdlc16 <joshdlc1601@yahoo.com>
2025-09-30 01:47:12 -0500

    gun code KINDA

---
+++ Assets/Scripts/Gun/GunSystem.cs
@@ -0,0 +1,100 @@
+using TMPro;
+using UnityEngine;
+
+public class GunSystem : MonoBehaviour
+{
+    //gun statistics
+    public int damage;
+    public float timeBetweenShooting, spread, range, reloadTime, timeBetweenShots;
+    public int magazineSize, bulletsPerTap;
+    public bool allowButtonHold;
+    int bulletsLeft, bulletsShot;
+
+    //bools
+    bool shooting, readyToShoot, reloading;
+
+    //references
+    public Camera fpsCam;
+    public Transform attackPoint;
+    public RaycastHit rayHit;
+    public LayerMask whatIsEnemy;
+
+    //graphics
+    //public GameObject muzzleFlash, bulletHoleGraphic;
+    //public CamShake camShake;
+    //public float camShakeMagnitude, camShakeDuration;
+    public TextMeshProUGUI text;
+
+    private void Awake()
+    {
+        bulletsLeft = magazineSize;
+        readyToShoot = true;
+    }
+    private void Update()
+    {
+        MyInput();
+        text.SetText(bulletsLeft + " / " + magazineSize);
+    }
+
+    //shooting input
+    private void MyInput()
+    {
+        if (allowButtonHold) shooting = Input.GetKey(KeyCode.Mouse0);
+        else shooting = Input.GetKeyDown(KeyCode.Mouse0);
+
+        if (Input.GetKeyDown(KeyCode.R) && bulletsLeft < magazineSize && !reloading) Reload();
+
+        //shooting
+        if (readyToShoot && shooting && !reloading && bulletsLeft > 0)
+        {
+            bulletsShot = bulletsPerTap;
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        readyToShoot = false;
+
+        //bullet spread
+        float x = Random.Range(-spread, spread);
+        float y = Random.Range(-spread, spread);
+
+        //calculating direction with spread
+        Vector3 direction = fpsCam.transform.forward + new Vector3(x, y, 0);
+
+        //raycast
+        if (Physics.Raycast(fpsCam.transform.position, direction, out rayHit, range, whatIsEnemy))
+        {
+            Debug.Log(rayHit.collider.name);
+
+            //if (rayHit.collider.CompareTag("Enemy"))
+               // rayHit.collider.GetComponent<ShootingAi>().TakeDamage(damage);
+        }
+
+        bulletsLeft--;
+        bulletsShot--;
+
+        Invoke("ResetShot", timeBetweenShooting);
+
+        if (bulletsShot > 0 && bulletsLeft > 0)
+            Invoke("Shoot", timeBetweenShots);
+    }
+
+    private void ResetShot()
+    {
+        readyToShoot = true;
+    }
+
+    //reloading
+    private void Reload()
+    {
+        reloading = true;
+        Invoke("ReloadFinished", reloadTime);
+    }
+    private void ReloadFinished()
+    {
+        bulletsLeft = magazineSize;
+        reloading = false;
+    }
+}
