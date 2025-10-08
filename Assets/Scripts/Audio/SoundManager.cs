6218c6432bcb15732b191c7fadf7002228f0202c
LariBates <laribates31@outlook.com>
2025-10-07 19:07:38 -0500

    Added SFX manager into test scene

---
+++ Assets/Scripts/Audio/SoundManager.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance;
+
+    public AudioClip[] sfxList;
+    public List<AudioResource> audioResources = new List<AudioResource>();
+
+    public GameObject sfxOneShotPrefab;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void OnEnable()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    public void PlaySFX(int index, float pitchMultiplier = 1)
+    {
+        GameObject sfx;
+        
+        
+        sfx = Instantiate(sfxOneShotPrefab, transform.position, Quaternion.identity);
+        
+        
+        AudioSource audioSource = sfx.GetComponent<AudioSource>();
+        audioSource.pitch = audioSource.pitch * pitchMultiplier;
+        sfx.GetComponent<SFXOneShotPrefab>().PlaySFXOneShot(sfxList[index]);
+    }
+
+    public void PlayAudioResource(int index, float pitchMultiplier = 1)
+    {
+        print("sound manager playing audio resource");
+        var sfx = Instantiate(sfxOneShotPrefab, transform.position, Quaternion.identity);
+        AudioSource audioSource = sfx.GetComponent<AudioSource>();
+        audioSource.pitch = audioSource.pitch * pitchMultiplier;
+        sfx.GetComponent<SFXOneShotPrefab>().PlaySFXOneShot(audioResources[index]);
+    }
+}
