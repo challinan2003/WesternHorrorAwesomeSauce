12c2fd60a4c808216ff7819949b52118e77e74f8
joshdlc16 <joshdlc1601@yahoo.com>
2025-10-14 06:28:22 -0500

    item pick up kind of works

--- Assets/Scripts/Items/InventoryManager.cs
+++ Assets/Scripts/Items/InventoryManager.cs
@@ -6,6 +6,7 @@ public class InventoryManager : MonoBehaviour
 {
     public GameObject InventoryMenu;
     private bool menuActivated;
+    public ItemSlot[] itemSlot;
 
     void Update()
     {
@@ -23,6 +24,18 @@ public class InventoryManager : MonoBehaviour
             Time.timeScale = 0;
             InventoryMenu.SetActive(true);
             menuActivated = true;
+        }
+    }
+
+    public void AddItem(string itemName, int quantity, Sprite itemSprite)
+    {
+        for (int i = 0; i < itemSlot.Length; i++)
+        {
+            if(itemSlot[i].isFull == false)
+            {
+                itemSlot[i].AddItem(itemName, quantity, itemSprite);
+                return;
+            }
         }
     }
 }
---
+++ Assets/Scripts/Items/Item.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    [SerializeField]
+    private string itemName;
+
+    [SerializeField]
+    private int quantity;
+
+    [SerializeField]
+    private Sprite sprite;
+
+    private InventoryManager inventoryManager;
+
+    void Start()
+    {
+        inventoryManager = GameObject.Find("InventoryCanvas").GetComponent<InventoryManager>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player");
+        {
+            inventoryManager.AddItem(itemName, quantity, sprite);
+            Destroy(gameObject);
+        }
+    }
+}
