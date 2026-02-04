faabebf181f0d39b2c8c035e57181bdd46b097f2
allyssa <allyssa.i.vasquez@gmail.com>
2026-02-04 02:04:52 -0600

    small change

--- Assets/Scripts/Audio/EnemySoundManager.cs
+++ Assets/Scripts/Audio/EnemySoundManager.cs
@@ -5,46 +5,46 @@ using UnityEngine.Audio;
 
 public class EnemySoundManager : MonoBehaviour
 {
-    public static EnemySoundManager instance;
+    //public static EnemySoundManager instance;
 
-    public AudioClip[] sfxList;
-    public List<AudioResource> audioResources = new List<AudioResource>();
+    //public AudioClip[] sfxList;
+    //public List<AudioResource> audioResources = new List<AudioResource>();
 
-    public GameObject enemySFXOneShotPrefab;
-
-
-    private void OnEnable()
-    {
-        if (instance == null)
-        {
-            instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
-    public void PlaySFX(int index, float pitchMultiplier = 1)
-    {
-        GameObject sfx;
+    //public GameObject enemySFXOneShotPrefab;
 
 
-        sfx = Instantiate(enemySFXOneShotPrefab, transform.position, Quaternion.identity);
+    //private void OnEnable()
+    //{
+    //    if (instance == null)
+    //    {
+    //        instance = this;
+    //        DontDestroyOnLoad(gameObject);
+    //    }
+    //    else
+    //    {
+    //        Destroy(gameObject);
+    //    }
+    //}
+    //public void PlaySFX(int index, float pitchMultiplier = 1)
+    //{
+    //    GameObject sfx;
 
 
-        AudioSource audioSource = sfx.GetComponent<AudioSource>();
-        audioSource.pitch = audioSource.pitch * pitchMultiplier;
-        sfx.GetComponent<SFXOneShotPrefab>().PlaySFXOneShot(sfxList[index]);
-    }
+    //    sfx = Instantiate(enemySFXOneShotPrefab, transform.position, Quaternion.identity);
 
-    public void PlayAudioResource(int index, float pitchMultiplier = 1)
-    {
-        print("sound manager playing audio resource");
-        var sfx = Instantiate(enemySFXOneShotPrefab, transform.position, Quaternion.identity);
-        AudioSource audioSource = sfx.GetComponent<AudioSource>();
-        audioSource.pitch = audioSource.pitch * pitchMultiplier;
-        sfx.GetComponent<EnemySFXScript>().PlaySFXOneShot(audioResources[index]);
-    }
+
+    //    AudioSource audioSource = sfx.GetComponent<AudioSource>();
+    //    audioSource.pitch = audioSource.pitch * pitchMultiplier;
+    //    sfx.GetComponent<SFXOneShotPrefab>().PlaySFXOneShot(sfxList[index]);
+    //}
+
+    //public void PlayAudioResource(int index, float pitchMultiplier = 1)
+    //{
+    //    print("sound manager playing audio resource");
+    //    var sfx = Instantiate(enemySFXOneShotPrefab, transform.position, Quaternion.identity);
+    //    AudioSource audioSource = sfx.GetComponent<AudioSource>();
+    //    audioSource.pitch = audioSource.pitch * pitchMultiplier;
+    //    sfx.GetComponent<EnemySFXScript>().PlaySFXOneShot(audioResources[index]);
+    //}
 }
 
